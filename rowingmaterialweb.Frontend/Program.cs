d02eb116cb58a08cd047df84cb99465834333185
LuisPulenta <luisalbertonu@gmail.com>
2024-05-26 11:49:49 -0300

    Projects Create

--- rowingmaterialweb.Frontend/Program.cs
+++ rowingmaterialweb.Frontend/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using rowingmaterialweb.Fronend;
+using rowingmaterialweb.Frontend;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
